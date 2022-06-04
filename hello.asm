4727ba66f52e631c841678fc10862c7256630b0b
Jeff Linahan <jeff.linahan@gmail.com>
2022-06-04 13:53:33 -0400

    undo accidental stz to INIDISP

--- hello.asm
+++ hello.asm
@@ -45,6 +45,9 @@ start:
    dex
    bpl @loop
 
+   lda #128
+   sta INIDISP ; undo the accidental stz to 2100h due to BPL actually being a branch on nonnegative
+
    ; Set palette to black background and 3 shades of red
    stz CGADD ; start with color 0 (background)
    stz CGDATA ; None more black
